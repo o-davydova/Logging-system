9dbd74e03adb45a9222d6fcbfc7a239bb3a32161
Artem Chuniak <64414765+Artem4ikPE@users.noreply.github.com>
2024-06-04 22:17:43 +0300

    Update LoggingLibrary.cpp

--- src/LoggingLibrary/LoggingLibrary.cpp
+++ src/LoggingLibrary/LoggingLibrary.cpp
@@ -1,23 +1,21 @@
-// LoggingLibrary.cpp
-#include "LoggingLibrary.h"
-#include <iostream>
-#include <ctime>
-#include <cstring>
+#include <chrono>
 #include <iomanip>
+#include <iostream>
+#include <LoggingLibrary.hpp>
 #include <sstream>
 #ifdef _WIN32
-#include <winsock2.h>
 #include <ws2tcpip.h>
+#include <winsock2.h>
 #pragma comment(lib, "Ws2_32.lib")
 #else
 #include <arpa/inet.h>
 #include <unistd.h>
 #endif
 
-#define PORT 8080
+constexpr int port = 8080;
 
 // Initialize static member
-LoggingLibrary::LogOutput LoggingLibrary::currentLogOutput = LoggingLibrary::BOTH;
+LogOutput LoggingLibrary::currentLogOutput = LogOutput::BOTH;
 
 void LoggingLibrary::logMessage(const std::string& message, LogLevel level, const std::string& client_name) {
     int sock = 0;
@@ -37,7 +35,7 @@ void LoggingLibrary::logMessage(const std::string& message, LogLevel level, cons
     }
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_port = htons(port);
 
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
         std::cerr << "Invalid address/ Address not supported" << std::endl;
@@ -49,21 +47,27 @@ void LoggingLibrary::logMessage(const std::string& message, LogLevel level, cons
         return;
     }
 
-    // Get current time
-    time_t now = time(0);
-    char* dt = ctime(&now);
-    dt[strlen(dt) - 1] = '\0';  // Remove newline character
+    // get time std::chrono
+    auto now = std::chrono::system_clock::now();
+    std::time_t time = std::chrono::system_clock::to_time_t(now);
+
+    // format time std::put_time
+    std::tm local_time = *std::localtime(&time);
+    std::ostringstream time_stream;
+    time_stream << std::put_time(&local_time, "%Y-%m-%d %H:%M:%S");
+    std::string dt = time_stream.str(); 
+
 
     // Form the log message
     std::string log_level;
     switch (level) {
-    case INFO_LOG:
+    case LogLevel::INFO_LOG:
         log_level = "[INFO]";
         break;
-    case WARNING_LOG:
+    case LogLevel::WARNING_LOG:
         log_level = "[WARNING]";
         break;
-    case ERROR_LOG:
+    case LogLevel::ERROR_LOG:
         log_level = "[ERROR]";
         break;
     default:
@@ -84,25 +88,13 @@ void LoggingLibrary::logMessage(const std::string& message, LogLevel level, cons
         << message;
     std::string str_message = formatted_message.str();
 
-    bool writeToFile = true;
-    bool writeToConsole = true;
+    bool writeToFile = (currentLogOutput == LogOutput::FILE || currentLogOutput == LogOutput::BOTH);
+    bool writeToConsole = (currentLogOutput == LogOutput::CONSOLE || currentLogOutput == LogOutput::BOTH);
 
-    if (currentLogOutput == FILE)
-    {
-        writeToFile = true;
-        writeToConsole = false;
-    }
-    else if (currentLogOutput == CONSOLE)
-    {
-        writeToFile = false;
-        writeToConsole = true;
-    }
     // Send log message along with output settings
     send(sock, str_message.c_str(), str_message.size(), 0);
     send(sock, reinterpret_cast<const char*>(&writeToConsole), sizeof(bool), 0);
     send(sock, reinterpret_cast<const char*>(&writeToFile), sizeof(bool), 0);
-
-
 
 #ifdef _WIN32
     closesocket(sock);
